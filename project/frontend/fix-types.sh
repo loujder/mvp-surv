ef8a5d84af4c59119dcba19a81ad8bc9e10ed822
loujder <ptararoev@gmail.com>
2025-07-21 02:20:00 +0300

    Delete project directory

--- project/frontend/fix-types.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-find node_modules/@types/node -name '*.d.ts' -exec sed -i "s|'undici-types'|\"undici-types\"|g" {} \;
-sed -i "s/throw err;//g" node_modules/vue-tsc/bin/vue-tsc.js
--- project/frontend/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'undici-types' {
-  // Пустая декларация для обхода ошибки
-}
